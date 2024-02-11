7d68a7baed69a6ecad8b173be697b3a61e053f17
Yuri Lima <yurilima742@yahoo.com>
2024-02-11 10:39:38 -0300

    refact: Mostrando snackbar de erro

--- lib/features/contacts_cubit/list/contacts_list_cubit_page.dart
+++ lib/features/contacts_cubit/list/contacts_list_cubit_page.dart
@@ -23,77 +23,98 @@ class ContactsListCubitPage extends StatelessWidget {
       ),
 
 
-      body: RefreshIndicator(
-        onRefresh: () => context.read<ContactListCubit>().findAll(),
+      body: BlocListener<ContactListCubit, ContactListCubitState>(
+        listenWhen: (previous, current) => current.maybeWhen(
+          error: (error) => true,
+          orElse: () => false,
+        ),
 
-        child: CustomScrollView(
-          slivers: [
-            SliverFillRemaining(
-              child: Column(
-                children: [
-                  Loader<ContactListCubit, ContactListCubitState>(
-                    selector: (state) => state.maybeWhen(
-                      loading: () => true,
-                      orElse: () => false,
+        listener: (context, state) {
+          state.whenOrNull(
+            error: (error) => ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  error,
+                  style: const TextStyle(color: Colors.white),
+                ),
+                backgroundColor: Colors.red,
+              ),
+            ),
+          );
+        },
+
+        child: RefreshIndicator(
+          onRefresh: () => context.read<ContactListCubit>().findAll(),
+  
+          child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                child: Column(
+                  children: [
+                    Loader<ContactListCubit, ContactListCubitState>(
+                      selector: (state) => state.maybeWhen(
+                        loading: () => true,
+                        orElse: () => false,
+                      ),
                     ),
-                  ),
-        
-                  BlocSelector<ContactListCubit, ContactListCubitState,
-                      List<ContactModel>>(
-        
-                    selector: (state) {
-                      return state.maybeWhen(
-                        data: (contacts) => contacts,
-                        orElse: () => <ContactModel>[],
-                      );
-                    },
-        
-                    builder: (_, contacts) => ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: contacts.length,
-        
-                      itemBuilder: (_, index) {
-                        final contact = contacts[index];
-        
-                        return ListTile(
-                          onTap: () async {
-                            await Navigator.pushNamed(
-                              context,
-                              '/contacts/cubit/update',
-                              arguments: contacts[index],
-                            );
-                            
-                            context
-                                .read<ContactListCubit>().findAll();
-                          },
-
-                          title: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                            children: [
-                              Text(contact.name),
-
-                              IconButton(
-                                onPressed: () => context
-                                    .read<ContactListCubit>()
-                                    .deleteByModel(contact),
-                                        
-                                icon: const Icon(Icons.delete),
-                                visualDensity: VisualDensity.compact,
-                              ),
-                            ],
-                          ),
-                          
-                          subtitle: Text(contact.email),
+          
+                    BlocSelector<ContactListCubit, ContactListCubitState,
+                        List<ContactModel>>(
+          
+                      selector: (state) {
+                        return state.maybeWhen(
+                          data: (contacts) => contacts,
+                          orElse: () => <ContactModel>[],
                         );
                       },
+          
+                      builder: (_, contacts) => ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: contacts.length,
+          
+                        itemBuilder: (_, index) {
+                          final contact = contacts[index];
+          
+                          return ListTile(
+                            onTap: () async {
+                              await Navigator.pushNamed(
+                                context,
+                                '/contacts/cubit/update',
+                                arguments: contacts[index],
+                              );
+                              
+                              context
+                                  .read<ContactListCubit>().findAll();
+                            },
+  
+                            title: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  
+                              children: [
+                                Text(contact.name),
+  
+                                IconButton(
+                                  onPressed: () => context
+                                      .read<ContactListCubit>()
+                                      .deleteByModel(contact),
+                                          
+                                  icon: const Icon(Icons.delete),
+                                  visualDensity: VisualDensity.compact,
+                                ),
+                              ],
+                            ),
+                            
+                            subtitle: Text(contact.email),
+                          );
+                        },
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
